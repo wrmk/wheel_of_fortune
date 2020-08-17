86d48d9c564a6b6853be8da67541c3ab09de07d1
wrmk <elmakan@gmail.com>
2020-08-17 19:41:23 +0300

    new and index view, modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: "main_scene#index" # попадание по умолчанию на эту страницу
 
+  resource :main_scene #создаём REST пути
+  get 'new' => 'main_scene#new' #переадресация страниц
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
