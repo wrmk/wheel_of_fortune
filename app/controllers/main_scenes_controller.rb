45528cf786bdb8bc145b69c5011e3fff98fd9d87
wrmk <elmakan@gmail.com>
2020-08-19 20:26:00 +0300

    some changes for save params in future

--- app/controllers/main_scenes_controller.rb
+++ app/controllers/main_scenes_controller.rb
@@ -1,2 +1,26 @@
 class MainScenesController < ApplicationController
+
+  def new
+  end
+
+  
+  def create
+    # @@word = params[:word]
+    # render action: 'index'
+
+    render plain: params[:word].inspect
+  end
+
+  def show
+      @sa == @@word
+  end
+
+
+
+  private 
+  #какие параметры можно тянуть со страниц
+  # def main_scene_params
+  #   params.require(:main_scene).permit(:word)
+  # end
+
 end
