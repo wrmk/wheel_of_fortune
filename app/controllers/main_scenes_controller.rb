3eeed3f7a72e98a00d4020b69dc216517077ba1b
wrmk <elmakan@gmail.com>
2020-08-31 20:05:31 +0300

    create hash with secret word and "X" for table

--- app/controllers/main_scenes_controller.rb
+++ app/controllers/main_scenes_controller.rb
@@ -1,11 +1,19 @@
 class MainScenesController < ApplicationController
 
   def init_table
-    #проверка загадано ли слово, если да, создавать на форме табло
-    #с удалением пробелов и ентер
-    defined?(@@secret_word) ? @table = @@secret_word.chomp.gsub(/ /,'').split(//) : @table = ''
+    #проверка загадано ли слово, если да 
+    if defined?(@@secret_word) then
+      #удаляем пробелы и ентер и создаем массив букв
+      @table = @@secret_word.chomp.gsub(/ /,'').split(//)
+      #создаем хэш с 
+      @table_hash = {}
+      @table.each do |c|
+        @table_hash[c] = 'X'
+      end
     
-
+    else
+      @table = ''
+    end
   end
 
   def new
@@ -34,7 +42,6 @@ class MainScenesController < ApplicationController
 
   def update
     @char = main_scene_params[:char]
-
   end
 
 
